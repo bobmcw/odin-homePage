9d46399debca020b0625dc4ba057c0c6103651ce
bobmcw <jasiekz2014@gmail.com>
2024-10-02 10:52:10 +0200

    addded a placeholder img

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css"
+import imgSmall from "./img/img_640w.jpg"
+import imgBig from "./img/img_1280w.jpg"
+
+
+const myImage = document.querySelector("img.myImg")
+myImage.srcset = `${imgSmall} 640w, ${imgBig} 1280w`
