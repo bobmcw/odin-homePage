37d9f5903d1660c1172d40a83c36eb044b1a64ce
bobmcw <jasiekz2014@gmail.com>
2024-10-10 13:25:52 +0200

    added rendering multiple project cards in js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ import imgBig from "./img/img_1280w.jpg"
 
 const myImage = document.querySelector("img.myImg")
 myImage.srcset = `${imgSmall} 640w, ${imgBig} 1280w`
+
+const myWork = document.querySelector(".myWork")
+const card = document.querySelector(".card")
+for(let i =0;i<=4;i++){
+    const addCard = card.cloneNode(true)
+    myWork.appendChild(addCard)
+}
