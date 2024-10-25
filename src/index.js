ab85e77cd5fcbca80e74b0c7ab2c0e13762ba3d4
bobmcw <jasiekz2014@gmail.com>
2024-10-25 14:33:57 +0200

    added a fiiter img, imoroved footer style and added a footer wrapper

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "./style.css"
 import imgSmall from "./img/img_640w.jpg"
 import imgBig from "./img/img_1280w.jpg"
+import footerImg from "./img/footerImg.jpg"
 
 
 const myImage = document.querySelector("img.myImg")
 myImage.srcset = `${imgSmall} 640w, ${imgBig} 1280w`
+const myFooterImg = document.querySelector(".footerImg")
+myFooterImg.src = footerImg
 
 const myWork = document.querySelector(".myWork")
 const card = document.querySelector(".card")
